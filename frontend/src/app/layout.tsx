796f7942c81bbce8629c7f1bad8d1c44b342da75
Trinh The Nhat <trinhnhat1908@gmail.com>
2024-12-24 00:13:02 +0700

    Connect MetaMask

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -22,7 +22,10 @@ export default function RootLayout({
         <body
           className={`${inter.className} text-[#EAECEF] bg-[#202328]`}
         >
-          {children}
+          <div className="relative bg-custom-image bg-no-repeat bg-center bg-cover bg-fixed">
+            <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-black/20"></div>
+              {children}           
+          </div>
         </body>
       </WalletContextProvider>
     </html>
